3b2a49d4056d9b67c9e0a4547ffc63b3d5fb2b6d
edwardlanto <edwardlanto@hotmail.com>
2018-01-23 17:21:17 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,18 +4,19 @@
  *
  * @package RED_Starter_Theme
  */
-
 ?>
 
-			</div><!-- #content -->
+            </div><!-- #content -->
 
-			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-				</div><!-- .site-info -->
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
+            <footer id="colophon" class="site-footer" role="contentinfo">
+                <div class="site-info">
+                </div><!-- .site-info -->
+            </footer><!-- #colophon -->
+        </div><!-- #page -->
 
-		<?php wp_footer(); ?>
-		<script src="https://use.fontawesome.com/51000e27a8.js"></script>
-	</body>
+        <?php
+wp_footer();
+?>
+       <script src="https://use.fontawesome.com/51000e27a8.js"></script>
+    </body>
 </html>
